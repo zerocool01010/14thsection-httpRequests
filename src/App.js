1bb94e042df5cd2b17e7aa4400c43d3b61febb41
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-04-29 10:43:47 -0300

    Managing errors in a http request and rendering conditionally depending the status code received as a server response

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
+  const emptyMoviesArray = []
   const dummyMovies = [
     {
       id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
+      title: "Some Dummy Movie",
+      openingText: "This is the opening text of the movie",
+      releaseDate: "2021-05-18",
     },
     {
       id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
+      title: "Some Dummy Movie 2",
+      openingText: "This is the second opening text of the movie",
+      releaseDate: "2021-05-19",
     },
-  ]; 
+  ];
 
-  const [movies, setMovies] = useState(dummyMovies)
+  const [movies, setMovies] = useState(dummyMovies);
+  const [isLoading, setIsLoading] = useState(false)
+  const [anError, setError] = useState(false)
 
   async function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map(movieInfo => {
-          return {
-            id: movieInfo.episode_id,
-            title: movieInfo.title,
-            director: movieInfo.director,
-            producer: movieInfo.producer,
-            openingText: movieInfo.opening_crawl,
-            releaseDate: movieInfo.release_date
-          }
-        })
-        setMovies(transformedMovies);
-      });
+    setIsLoading(true)
+    try {
+    setError(null)
+    const resp = await fetch("https://swapi.dev/api/films");
+
+    if (!(resp.ok)) {
+      throw new Error('Something went wrong: '+resp.status)
+    }
+
+    const data = await resp.json()
+
+    const transformedMovies = data.results.map((movieInfo) => {
+      return {
+        id: movieInfo.episode_id,
+        title: movieInfo.title,
+        director: movieInfo.director,
+        producer: movieInfo.producer,
+        openingText: movieInfo.opening_crawl,
+        releaseDate: movieInfo.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    } catch (error) {
+      setMovies([])
+      setError(error.message)
+    }
+    setIsLoading(false)
   }
 
   return (
@@ -47,7 +60,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+      {/* {(isLoading || movies.length === 0) ? <h2>Loading data, please wait...</h2> : <MoviesList movies={movies} />}  */}
+      {isLoading && <h2>Loading data, please wait...</h2>}
+      {(!isLoading && movies.length === 0 && !anError) && <h2>Movies not found</h2>} {/* si uso el emptyMoviesArray para setear el setMovies puedo comprobar esta condicion */}
+      {(!isLoading && anError) && <p>{anError}</p>} 
+      {!isLoading && <MoviesList movies={movies}/>}
       </section>
     </React.Fragment>
   );
