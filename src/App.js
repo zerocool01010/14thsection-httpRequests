14064c81dd65f252142389fa7d8eb3e098e7bf92
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-04-29 11:06:13 -0300

    Adding useEffect hook to fetch automatically when the app loads for first time, avoiding loops

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const emptyMoviesArray = []
+  const emptyMoviesArray = [];
   const dummyMovies = [
     {
       id: 1,
@@ -21,38 +21,42 @@ function App() {
   ];
 
   const [movies, setMovies] = useState(dummyMovies);
-  const [isLoading, setIsLoading] = useState(false)
-  const [anError, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [anError, setError] = useState(false);
 
-  async function fetchMoviesHandler() {
-    setIsLoading(true)
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
     try {
-    setError(null)
-    const resp = await fetch("https://swapi.dev/api/films");
+      setError(null);
+      const resp = await fetch("https://swapi.dev/api/films");
 
-    if (!(resp.ok)) {
-      throw new Error('Something went wrong: '+resp.status)
-    }
+      if (!resp.ok) {
+        throw new Error("Something went wrong: " + resp.status);
+      }
 
-    const data = await resp.json()
+      const data = await resp.json();
 
-    const transformedMovies = data.results.map((movieInfo) => {
-      return {
-        id: movieInfo.episode_id,
-        title: movieInfo.title,
-        director: movieInfo.director,
-        producer: movieInfo.producer,
-        openingText: movieInfo.opening_crawl,
-        releaseDate: movieInfo.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      const transformedMovies = data.results.map((movieInfo) => {
+        return {
+          id: movieInfo.episode_id,
+          title: movieInfo.title,
+          director: movieInfo.director,
+          producer: movieInfo.producer,
+          openingText: movieInfo.opening_crawl,
+          releaseDate: movieInfo.release_date,
+        };
+      });
+      setMovies(transformedMovies);
     } catch (error) {
-      setMovies([])
-      setError(error.message)
+      setMovies([]);
+      setError(error.message);
     }
-    setIsLoading(false)
-  }
+    setIsLoading(false);
+  }, [])
+
+  useEffect(()=> {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
 
   return (
     <React.Fragment>
@@ -60,11 +64,14 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-      {/* {(isLoading || movies.length === 0) ? <h2>Loading data, please wait...</h2> : <MoviesList movies={movies} />}  */}
-      {isLoading && <h2>Loading data, please wait...</h2>}
-      {(!isLoading && movies.length === 0 && !anError) && <h2>Movies not found</h2>} {/* si uso el emptyMoviesArray para setear el setMovies puedo comprobar esta condicion */}
-      {(!isLoading && anError) && <p>{anError}</p>} 
-      {!isLoading && <MoviesList movies={movies}/>}
+        {/* {(isLoading || movies.length === 0) ? <h2>Loading data, please wait...</h2> : <MoviesList movies={movies} />}  */}
+        {isLoading && <h2>Loading data, please wait...</h2>}
+        {!isLoading && movies.length === 0 && !anError && (
+          <h2>Movies not found</h2>
+        )}{" "}
+        {/* si uso el emptyMoviesArray para setear el setMovies puedo comprobar esta condicion */}
+        {!isLoading && anError && <p>{anError}</p>}
+        {!isLoading && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
