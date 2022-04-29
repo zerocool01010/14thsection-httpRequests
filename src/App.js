da698c222b55d840e1fafce416dee30aa3083db2
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-04-29 15:49:53 -0300

    POST codeado con un form con una url ficticia todavia, no va a funcar por el momento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
-import AddMoviesForm from './components/AddMovie'
+import AddMoviesForm from "./components/AddMovie";
 import "./App.css";
 
 function App() {
+  const theUrl = 'https://swapi.dev/api/'; //esta url sirve para el get, no para el post
   const emptyMoviesArray = [];
+  const loadedMovies = [] //para el POST
   const dummyMovies = [
     {
       id: 1,
@@ -29,7 +31,7 @@ function App() {
     setIsLoading(true);
     try {
       setError(null);
-      const resp = await fetch("https://swapi.dev/api/films");
+      const resp = await fetch(theUrl+'films');
 
       if (!resp.ok) {
         throw new Error("Something went wrong: " + resp.status);
@@ -47,22 +49,58 @@ function App() {
           releaseDate: movieInfo.release_date,
         };
       });
-      setMovies(transformedMovies);
+
+      if (!(loadedMovies.length > 0)) { //si no hay elementos en el arreglo de la pelicula del form
+        setMovies(transformedMovies);
+      } else {
+        setMovies(loadedMovies)
+        loadedMovies.pop()
+      }
+
     } catch (error) {
       setMovies([]);
       setError(error.message);
     }
     setIsLoading(false);
-  }, [])
+  }, []);
 
-  useEffect(()=> {
-    fetchMoviesHandler()
-  }, [fetchMoviesHandler])
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const postMovieHandler = useCallback(async (movie) => {
+    console.log(
+      "Aca esta la pelicula que queremos enviar por POST: ",
+      movie
+    );
+
+    const resp = await fetch(theUrl, { //la Url no esta bien definida aun, es solo el codigo, ya que solo sirve para el GET
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await resp.json() //en teoria en la url que deberiamos trabajar se devuelve un obj, no un array
+    console.log(data)
+
+    for (const key in data) { //el for in es para los obj
+      loadedMovies.push({
+        id: key,
+        title: data[key].title,
+        openingText: date[key].openingText,
+        date: date[key].releaseDate
+      })
+    }
+    setMovies(loadedMovies) //ahora vamos a ver las peli cargada
+
+  }, []);
 
   return (
     <React.Fragment>
       <section>
-        <AddMoviesForm/>
+        <AddMoviesForm onAddMovie={postMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -19,6 +19,9 @@ function AddMovie(props) {
     };
 
     props.onAddMovie(movie);
+    titleRef.current.value = ''
+    openingTextRef.current.value = ''
+    releaseDateRef.current.value = ''
   }
 
   return (
